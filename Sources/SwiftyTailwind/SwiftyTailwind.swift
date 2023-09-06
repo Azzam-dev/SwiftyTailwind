e97e42263b2c7c4e72de9cb017913adf3ee9416e
CAOCAP <ficruty@gmail.com>
2023-09-06 07:21:50 +0300

    added the swift package

---
+++ Sources/SwiftyTailwind/SwiftyTailwind.swift
@@ -0,0 +1,6 @@
+public struct SwiftyTailwind {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftyTailwindTests/SwiftyTailwindTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftyTailwind
+
+final class SwiftyTailwindTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftyTailwind().text, "Hello, World!")
+    }
+}
